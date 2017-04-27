f54805558ccd45dc5e5671b3b050f659faaa4719
Daniel D <djx339@gmail.com>
2017-04-27 16:47:28 +0800

    Optimize test step

--- tests/travis_test.sh
+++ tests/travis_test.sh
@@ -19,17 +19,13 @@ echo $docker_info | grep $old_version
 
 # Install latest docker
 export container_id=$(date +%s)
-playbook_opts="" ${PWD}/tests/test.sh
+playbook_opts='-e {"docker_opts":{"dns":["192.168.1.1","192.168.1.2"]},"docker_envs":{"http_proxy":"192.168.1.3"}}'
+playbook_opts="$playbook_opts" ${PWD}/tests/test.sh
 docker_info=$(docker exec --tty ${container_id} env TERM=xterm docker info)
 if ! echo $docker_info | grep $old_version >/dev/null 2>&1; then
     echo "Install latest docker successfully"
 fi
-
 # Testing docker opts
-export container_id=$(date +%s)
-playbook_opts='-e {"docker_opts":{"dns":["192.168.1.1","192.168.1.2"]},"docker_envs":{"http_proxy":"192.168.1.3"}}'
-playbook_opts="$playbook_opts" ${PWD}/tests/test.sh
-docker_info=$(docker exec --tty ${container_id} env TERM=xterm docker info)
 docker_conf=$(docker exec --tty ${container_id} env TERM=xterm cat /etc/docker/daemon.json)
 # Testing docker environment
 echo $docker_info | grep 192.168.1.3

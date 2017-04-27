eab1028fab87d4b3f5a2ab960cf17a4f0f9e0821
Daniel D <djx339@gmail.com>
2017-04-27 16:18:10 +0800

    Fix typo issue

--- tests/travis_test.sh
+++ tests/travis_test.sh
@@ -8,7 +8,7 @@ export skip_rerun=true
 
 old_version=17.03.0
 playbook_opts="-e docker_version=$old_version"
-playbook_opts="playbook_opts" ${PWD}/tests/test.sh
+playbook_opts="$playbook_opts" ${PWD}/tests/test.sh
 
 # Ensure docker is installed
 docker exec --tty ${container_id} env TERM=xterm which docker

e448e65af2a257ad8fe1550df9f418704eae37d9
Daniel D <djx339@gmail.com>
2017-04-27 16:07:25 +0800

    Update ci configuration

---
+++ tests/travis_test.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+set -e
+
+export container_id=$(date +%s)
+export cleanup=false
+export skip_rerun=true
+
+old_version=17.03.0
+playbook_opts="-e docker_version=$old_version"
+playbook_opts="playbook_opts" ${PWD}/tests/test.sh
+
+# Ensure docker is installed
+docker exec --tty ${container_id} env TERM=xterm which docker
+
+# Ensure specified version is installed
+docker_info=$(docker exec --tty ${container_id} env TERM=xterm docker info)
+echo $docker_info | grep $old_version
+
+# Install latest docker
+playbook_opts="" ${PWD}/tests/test.sh
+docker_info=$(docker exec --tty ${container_id} env TERM=xterm docker info)
+if ! echo $docker_info | grep $old_version >/dev/null 2>&1; then
+    echo "Installing latest docker and upgrading docker is successful"
+fi
+
+# Testing docker opts
+playbook_opts='-e {"docker_opts":{"dns":["192.168.1.1","192.168.1.2"]},"docker_envs":{"http_proxy":"192.168.1.3"}} --tags "configure"'
+playbook_opts="$playbook_opts" ${PWD}/tests/test.sh
+docker_info=$(docker exec --tty ${container_id} env TERM=xterm docker info)
+docker_conf=$(docker exec --tty ${container_id} env TERM=xterm cat /etc/docker/daemon.json)
+# Testing docker environment
+echo $docker_info | grep 192.168.1.3
+echo "=== Test change docker environemnt success ==="
+# Testing docker conf
+echo $docker_conf | grep 192.168.1.2
+echo "=== Test change docker configuration success ==="
